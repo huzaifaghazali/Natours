38d2d333ebf636020d66917ca57a151413dd428c
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-06-22 22:12:23 +0500

    Add smooth scroll functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
+document.addEventListener('DOMContentLoaded', function() {
+   // Smooth scroll to section
+   let toggleNavItems = document.querySelectorAll('.toggle-nav');
+ 
+   toggleNavItems.forEach(function(item) {
+     item.addEventListener('click', function(event) {
+       event.preventDefault();
+ 
+       let targetSection = item.firstElementChild.getAttribute('href');
+       let navToggleCheckbox = document.getElementById('navi-toggle');
+       
+       window.scrollTo({
+         top: document.querySelector(targetSection).offsetTop,
+         behavior: 'smooth'
+       });
+ 
+       // Close navigation bar
+       navToggleCheckbox.checked = false;
+     });
+   });
+ });
+ 
